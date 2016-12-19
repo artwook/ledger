317216de1ac0f0a9a6d0ca5ce655387e3b68d931
HackFisher <hackfisher@gmail.com>
2016-12-19 11:45:01 +0800

    Rename Graphene to Artwook

--- libraries/egenesis/embed_genesis.cpp
+++ libraries/egenesis/embed_genesis.cpp
@@ -221,7 +221,7 @@ void load_genesis(
 int main( int argc, char** argv )
 {
    int main_return = 0;
-   boost::program_options::options_description cli_options("Graphene Chain Identifier");
+   boost::program_options::options_description cli_options("Artwook Chain Identifier");
    cli_options.add_options()
       ("help,h", "Print this help message and exit.")
       ("genesis-json,g", boost::program_options::value<boost::filesystem::path>(), "File to read genesis state from")
